c025466c1afd6e9f6426452c334d822ab31caeb1
JoseDanielF <danielduartefilho.df@gmail.com>
2022-08-31 20:36:14 -0300

    Adicionando pacman

---
+++ pacman/pacman.dart
@@ -0,0 +1,25 @@
+import 'package:bonfire/bonfire.dart';
+
+import 'pacman_sprite.dart';
+
+class Pacman extends SimplePlayer with ObjectCollision {
+  Pacman(Vector2? position)
+      : super(
+          size: Vector2(32, 32),
+          animation: SimpleDirectionAnimation(
+            idleRight: PacmanSprite.idleRight,
+            runRight: PacmanSprite.runRight,
+            idleLeft: PacmanSprite.idleLeft,
+            runLeft: PacmanSprite.runLeft,
+            idleUp: PacmanSprite.idleUp,
+            runUp: PacmanSprite.runUp,
+            idleDown: PacmanSprite.idleDown,
+            runDown: PacmanSprite.runDown,
+          ),
+          position: position ?? Vector2(10, 10),
+          speed: 50,
+        ) {
+    setupCollision(CollisionConfig(
+        collisions: [CollisionArea.rectangle(size: Vector2(30, 30))]));
+  }
+}
